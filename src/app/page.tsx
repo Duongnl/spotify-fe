af5d4504a2c8fb1e67f37f80f6b4c6fac1a51483
Duongg <lengocduong003@gmail.com>
2025-03-11 15:23:45 +0700

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import { Button} from 'antd';
 const HomePage = () => {
   return (
     <>
-      <div className='p-7' >Home page</div>
-      <Button className='p-7' >Default Button</Button>
+       
     </>
   )
 }
