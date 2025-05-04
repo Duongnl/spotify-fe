7d54ee525112c4a460ce2ff28421cecc200e10e0
Duongg <lengocduong003@gmail.com>
2025-05-05 04:53:17 +0700

    login

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,6 @@
+import USER_API from "./endpoints/user";
+
 const API = {
-   
+    USER:USER_API
 };
 export default API;
---
+++ src/api/endpoints/user.ts
@@ -0,0 +1,6 @@
+
+const USER_API = {
+    LOGIN: `http://127.0.0.1:8000/login/`,
+  };
+  
+export default USER_API;
