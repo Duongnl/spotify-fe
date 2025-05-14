e2eef5f614f643d7e4e010bafaf42c4d99be33e7
Duongg <lengocduong003@gmail.com>
2025-05-14 07:29:23 +0700

    chat ai

--- src/api/endpoints/track.ts
+++ src/api/endpoints/track.ts
@@ -3,5 +3,6 @@ const BASE_URL = "http://127.0.0.1:8000";
 const TRACK_API = {
   GET_TRACK: (id: string) => `${BASE_URL}/tracks/${id}`,
   GET_TRACKS:`${BASE_URL}/tracks`,
+  GET_TRACK_ARTISTS :`${BASE_URL}/tracks/basic-with-artists/`
 };
 export default TRACK_API;
